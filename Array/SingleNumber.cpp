ef1011122985404c2a907852ae45132e540ebaf5
Surbhi Yadav <surbhiyadav10428@gmail.com>
2023-01-26 18:04:02 +0530

    Single Number

---
+++ Array/SingleNumber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size()-1){
+            int j=i+1;
+            if(nums[i]==nums[j]){
+                i=i+2;
+            }
+            else{
+                return nums[i];
+            }
+        }
+        return nums[i];
+    }
+};
